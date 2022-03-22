d53548361b27d36df4ea3f14d3ef91a47e7956f1
Raman Sharma <rs9463194297@gmail.com>
2022-03-22 11:27:37 +0530

    added buttons in initialize voice controls component

--- src/stories/components/InitializeVoiceControls/InitializeVoiceControls.tsx
+++ src/stories/components/InitializeVoiceControls/InitializeVoiceControls.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   faMicrophone,
   faMicrophoneSlash,
+  faTableCells,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { Button } from "../Button/Button";
@@ -10,16 +11,25 @@ import "./InitializeVoiceControls.css";
 
 export const InitializeVoiceControls = () => {
   const [isListening, setIsListening] = React.useState(false);
+  const [isInstrctionTableOpened, setIsInstrctionTableOpened] =
+    React.useState(false);
 
   return (
     <>
       <div className="initialize-voice-controls-box-layer">
         <Button
           className={`icon-button ${
-            isListening ? "button-mute" : "button-unmute"
+            isListening ? "button-unmute" : "button-mute"
           }`}
-          faIcon={isListening ? faMicrophoneSlash : faMicrophone}
+          faIcon={isListening ? faMicrophone : faMicrophoneSlash}
           onClick={() => setIsListening(!isListening)}
+        />
+        <Button
+          className={`icon-button ${
+            isInstrctionTableOpened ? "button-unmute" : "button-mute"
+          }`}
+          faIcon={faTableCells}
+          onClick={() => setIsInstrctionTableOpened(!isInstrctionTableOpened)}
         />
       </div>
     </>
